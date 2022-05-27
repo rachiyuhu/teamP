e9a8db7bbfc66feeb5a8dc43f66ba9747d9aa65d
junwookim <ekdlskalrx98@naver.com>
2022-05-27 17:52:10 +0900

    first commit

---
+++ upcycling/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <logo/>
+        <시작페이지 로그인 가입버튼/>
+        <소개/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ upcycling/src/context/1.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Item = ({ searchTerm }) => {
+    return (
+        <div>
+            <nav/>
+            <image/>
+            <게시글/>
+        </div>
+    );
+};
+
+export default Item;
